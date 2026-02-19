64c52d31d2041ccbf45a00ba8aa2bd550b31d893
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-02-20 02:12:35 +0530

    wrote test cases to find title from image teg

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <p>First react test case</p>
+      <img title='zomato' src="https://restaurantindia.s3.ap-south-1.amazonaws.com/s3fs-public/2023-02/zomato-infinity-dining-916x516-1_11zon.jpg" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -10,11 +10,20 @@ test("writting test case to find test",() => {
   expect(text).toBeInTheDocument();
 })
 
-test("writting test case to find test1",() => {
+test("writting test case to find some text",() => {
 
   render(<App/>);
 
   const text = screen.getByText(/First /i);
 
   expect(text).toBeInTheDocument();
+})
+
+test("writting test case to find title",() => {
+
+  render(<App/>);
+
+  const title = screen.getByTitle("zomato");
+
+  expect(title).toBeInTheDocument();
 })
