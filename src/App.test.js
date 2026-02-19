0b30b35673fca8bb8f98c09f6c946e59bc92d72b
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-02-20 02:03:47 +0530

    wrote test cases for checking text present or not

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("writting test case to find test",() => {
+
+  render(<App/>);
+
+  const text = screen.getByText("First react test case");
+
+  expect(text).toBeInTheDocument();
+})
