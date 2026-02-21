a011cf75ca1ba6a1295128e44f9f6253ef1d190e
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-02-22 01:37:04 +0530

    file and folder naming convention

---
+++ src/__tests__/Apple.js
@@ -0,0 +1,12 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import App from "../App";
+
+
+test("click event test case",() => {
+  render(<App/>);
+  const btn = screen.getByRole("button");
+
+  fireEvent.click(btn);
+
+  expect(screen.getByText("update data")).toBeInTheDocument();
+})
