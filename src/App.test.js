ec5af43c6c3f99509643aff7c20a37f249728316
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-02-22 00:39:08 +0530

    Describe grouping and read only another skiped

--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,32 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("writting test case to find test",() => {
-
-  render(<App/>);
+test("writting test case to find test", () => {
+  render(<App />);
 
   const text = screen.getByText("First react test case");
 
   expect(text).toBeInTheDocument();
-})
+});
 
-test("writting test case to find some text",() => {
-
-  render(<App/>);
+test("writting test case to find some text", () => {
+  render(<App />);
 
   const text = screen.getByText(/first/i);
 
   expect(text).toBeInTheDocument();
-})
+});
 
-test("writting test case to find title",() => {
-
-  render(<App/>);
+test.only("writting test case to find title", () => {
+  render(<App />);
 
   const title = screen.getByTitle("zomato");
 
   expect(title).toBeInTheDocument();
-})
+});
 
-test("test input box",() => {
-
-  render(<App/>);
+test("test input box", () => {
+  render(<App />);
 
   const textInput = screen.getByRole("textbox");
   expect(textInput).toBeInTheDocument();
@@ -38,6 +34,42 @@ test("test input box",() => {
   const placeholderInput = screen.getByPlaceholderText("input your name");
   expect(placeholderInput).toBeInTheDocument();
 
-  expect(textInput).toHaveAttribute("name","your name");
-  expect(textInput).toHaveAttribute("type","text");
-})
+  expect(textInput).toHaveAttribute("name", "your name");
+  expect(textInput).toHaveAttribute("type", "text");
+});
+
+describe("UI test cases group", () => {
+  test("test case 1", () => {
+    render(<App />);
+    const textInput = screen.getByRole("textbox");
+    expect(textInput).toHaveAttribute("name", "your name");
+  })
+    test("test case 2", () => {
+    render(<App />);
+    const textInput = screen.getByRole("textbox");
+    expect(textInput).toHaveAttribute("name", "your name");
+  })
+    test("test case 3", () => {
+    render(<App />);
+    const textInput = screen.getByRole("textbox");
+    expect(textInput).toHaveAttribute("name", "your name");
+  })
+});
+
+describe("Api test cases group", () => {
+  test("test case 1", () => {
+    render(<App />);
+    const textInput = screen.getByRole("textbox");
+    expect(textInput).toHaveAttribute("name", "your name");
+  })
+    test("test case 2", () => {
+    render(<App />);
+    const textInput = screen.getByRole("textbox");
+    expect(textInput).toHaveAttribute("name", "your name");
+  })
+    test("test case 3", () => {
+    render(<App />);
+    const textInput = screen.getByRole("textbox");
+    expect(textInput).toHaveAttribute("name", "your name");
+  })
+});
--- src/sum.test.js
+++
@@ -1,9 +0,0 @@
-import sum from "./sum";
-
-test("testing 2 argument in sum",() => {
-
-    const a=10;
-    const b=20;
-
-    expect(sum(a,b)).toBe(30);
-})
