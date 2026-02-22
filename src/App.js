1b3084ab099df01caa87d0bbcfa52078b537e194
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-02-23 01:13:00 +0530

    multiple elements with role custom role multiple elements with the same role issue multiple buttons with role multiple input box with role custom role

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ import handleOtherMethod from './helper';
 function App() {
   return (
     <div className="App">
-      <h1>getByRole</h1>
-      <input type="text" defaultValue="hello" disabled/>
-      <button>click me</button>
+      <h1>multiple items with role</h1>
+      <h1>custom role</h1>
+      <button>Click 1</button>
+      <button>Click 2</button>
+      <label htmlFor="input1" >User Name</label>
+      <input type="text" id="input1" />
+ <label htmlFor="input2">User age</label>
+<input type="text" id="input2" />
+      <div role="dummy">
+        dummy Text
+      </div>
+      <input type="text" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,19 @@
 import { render, screen } from "@testing-library/react"
 import App from "./App"
 
+test("getByRole testing", () => {
 
-
-test("get by role", () => {
   render(<App/>);
-  const inputField = screen.getByRole("textbox");
-  const btn = screen.getByRole("button");
+  const btn1 = screen.getByRole("button",{name:"Click 1"});
+  const btn2 = screen.getByRole("button",{name:"Click 2"});
+  const input1 = screen.getByRole("textbox",{name:"User Name"})
+  const input2 = screen.getByRole("textbox",{name:"User age"})
+  const dv1 = screen.getByRole("dummy");
 
-  expect(inputField).toBeInTheDocument();
-  expect(btn).toBeInTheDocument();
-  
-  expect(inputField).toHaveValue("hello");
-  expect(inputField).toBeDisabled();
+  expect(btn1).toBeInTheDocument();
+  expect(btn2).toBeInTheDocument();
+  expect(input1).toBeInTheDocument();
+  expect(input2).toBeInTheDocument();
+  expect(dv1).toBeInTheDocument();
+
 })
