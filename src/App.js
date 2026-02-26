0f61efdf3bde182fcfa1889d6280300da31528f0
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-02-27 00:22:00 +0530

    use of getAllByPlaceholderText

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
      <h1>RTL Query: ByPlacehlderText</h1>
      <input type="text" placeholder="enter username" defaultValue={"anil"}/>
+     <input type="text" placeholder="enter username" defaultValue={"anil"}/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("test case for single input field", () => {
+// test("test case for single input field", () => {
+//   render(<App />);
+//   const input = screen.getByPlaceholderText("enter username");
+//   expect(input).toBeInTheDocument();
+//   expect(input).toHaveValue("anil");
+// });
+
+test("test case for multiple input fields", () => {
   render(<App />);
-  const input = screen.getByPlaceholderText("enter username");
-  expect(input).toBeInTheDocument();
-  expect(input).toHaveValue("anil");
+  const input = screen.getAllByPlaceholderText("enter username");
+
+  for (let i = 0; i < input.length; i++) {
+    expect(input[i]).toBeInTheDocument();
+    expect(input[i]).toHaveValue("anil");
+  }
 });
