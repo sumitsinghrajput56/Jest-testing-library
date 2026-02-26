c79e102428c3bbc7b02f0cfbbda31503e460d705
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-02-27 00:16:06 +0530

    use of getByPlaceholderText

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>RTL query : getByLabelText</h1>
-     <label htmlFor="user-name" >Username</label>
-     <input type="text" id="user-name" defaultValue="anil"/>
-     <br></br>
-
-     <label htmlFor="skills">Skills</label>
-     <input type="checkbox" id="skills" defaultChecked={true}/>
-     
+     <h1>RTL Query: ByPlacehlderText</h1>
+     <input type="text" placeholder="enter username" defaultValue={"anil"}/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("getByLabelText test case 1", () => {
+test("test case for single input field", () => {
   render(<App />);
-
-  const input = screen.getByLabelText("Username");
+  const input = screen.getByPlaceholderText("enter username");
   expect(input).toBeInTheDocument();
   expect(input).toHaveValue("anil");
-
-});
-
-test("getByLabelText test case 2", () => {
-  render(<App />);
-
-  const input = screen.getByLabelText("Skills");
-  expect(input).toBeInTheDocument();
-  expect(input).toBeChecked("anil");
-
 });
